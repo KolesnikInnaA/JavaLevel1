ffa08ab3471240def8e6a793184247dea8cb3a55
Inna Kolesnik <stutv@mail.ru>
2021-09-11 17:46:36 +0300

    homework for lesson4

---
+++ src/Lesson6/HomeWork6/Dog.java
@@ -0,0 +1,13 @@
+package Lesson6.HomeWork6;
+
+public class Dog extends Animal{
+
+        public Dog (String name, String run, String swim){
+        this.name = name;
+        this.runMax = 500;
+        this.swimMax = 10;
+    }
+
+}
+
+
---
+++ src/Lesson6/HomeWork6/Vork6.java
@@ -0,0 +1,24 @@
+package Lesson6.HomeWork6;
+
+public class Vork6 {
+
+
+    public static void main(String[] args) {
+
+    Cat catBasik = new Cat ("Basik ");
+        catBasik.run(10);
+        catBasik.swim(-1);
+
+
+
+    Dog dogTyavsik = new Dog ( "Tyavsik ", "Basik run " , "Basik swim " );
+        dogTyavsik.run(12); // method
+        dogTyavsik.swim(5);
+
+        }
+
+    }
+
+
+
+
